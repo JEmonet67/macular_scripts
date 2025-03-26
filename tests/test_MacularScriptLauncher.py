36fa14499ac4621c4eefa3009fa6fb785d55f224
Emonet <jerome.emonet.67@gmail.com>
2025-03-26 11:32:49 +0100

    Remove todo

--- tests/test_MacularScriptLauncher.py
+++ tests/test_MacularScriptLauncher.py
@@ -1,12 +1,6 @@
 import src.MacularScriptLauncher as msl
 import os
 
-# TODO : Test de comparaison de csv obtenu par le script comparé à un csv test conservé
-# TODO : Test sans path stim, sans path graph ou sans param=value
-# TODO : Faire les tests des erreurs levées dans la classe MacularScriptLauncher
-# TODO : Test simulation rétine : 1 seul str sans rien et n_stim=3, 1 sim avec graph/stim/params pas liste,
-#  1 avec liste de longueur 1, 3 simulations avec graph, stim et param sous forme de liste.
-# TODO : Que se passe t'il si n_sim = 0?
 
 def test_init_macular_script_launcher():
     path_init_file = "../resources/config_macuscript_default.json"
@@ -39,6 +33,7 @@ def test_init_macular_script_launcher():
     assert macuscript_launcher.params_dict == {}
     assert macuscript_launcher.dict_formatting_alias == {}
     assert macuscript_launcher.dict_config == dict_config_test
+
 
 def test_check_params_dict():
     path_init_file = "../resources/config_macuscript_default.json"
@@ -73,9 +68,9 @@ def test_change_dict_config():
     macuscript_launcher = msl.MacularScriptLauncher(path_init_file)
 
     dict_config_test = {'n_sim': 8, 'path_macufile': '/user/jemonet/home/Documents/These/Macular files/Session '
-                                                      'retinocortical/RefactoredMacular/diSymGraph_pConnecParams/'
-                                                      '9x3,15°_N41x15_nue0_1,86_nui0_12,66_horizWhiteBarMotion0,67x0,9°'
-                                                      '_6dps_deltat0,0167_ampOPL0,025.json',
+                                                     'retinocortical/RefactoredMacular/diSymGraph_pConnecParams/'
+                                                     '9x3,15°_N41x15_nue0_1,86_nui0_12,66_horizWhiteBarMotion0,67x0,9°'
+                                                     '_6dps_deltat0,0167_ampOPL0,025.json',
                         'path_macudata': '/user/jemonet/home/Documents/These/Data Macular/RefactoredMacular/'
                                          'diSymGraph_pConnecParams/41x15c/horizontal_white_bar/bar0,67x0,9°_6dps/'
                                          'ampGang/RC_RM_dSGpCP{id_sim}_'
@@ -93,8 +88,9 @@ def test_change_dict_config():
 
     macuscript_launcher.dict_config = dict_config_test
     assert macuscript_launcher.n_sim == 8
-    assert macuscript_launcher.dict_config["params_dict"]['virtual_retina/relative_ampOPL'] == [0.00083, 0.004, 0.0079, 0.0119, 0.0159,
-                                                                           0.0198, 0.0292, 0.0333]
+    assert macuscript_launcher.dict_config["params_dict"]['virtual_retina/relative_ampOPL'] == [0.00083, 0.004, 0.0079,
+                                                                                                0.0119, 0.0159,
+                                                                                                0.0198, 0.0292, 0.0333]
     assert macuscript_launcher.dict_config["value_caract_sim"] == [1, 5, 10, 15, 20, 25, 35, 40]
 
 
@@ -137,7 +133,6 @@ def test_refresh_dict_formatting_alias():
 
 
 def test_refresh_paths():
-    # TODO test avec une liste de macugraph pour diversifier un peu plus le test, en faire un autre pour tester l'absence de stim/graph
     path_init_file = "../resources/config_macuscript_default.json"
     macuscript_launcher = msl.MacularScriptLauncher(path_init_file)
 
@@ -159,16 +154,15 @@ def test_refresh_paths():
 
 
 def test_create_non_existing_path():
-    # TODO
     path_init_file = "../resources/config_macuscript_default.json"
     macuscript_launcher = msl.MacularScriptLauncher(path_init_file)
     path_macudata_file = "/user/jemonet/home/Documents/These/Data Macular/RefactoredMacular/" \
-                    "diSymGraph_pConnecParams/41x15c/horizontal_white_bar/bar0,67x0,9°_6dps/" \
-                    "ampGang/test/oui/test.py"
+                         "diSymGraph_pConnecParams/41x15c/horizontal_white_bar/bar0,67x0,9°_6dps/" \
+                         "ampGang/test/oui/test.py"
 
     path_macudata_dir = "/user/jemonet/home/Documents/These/Data Macular/RefactoredMacular/" \
-                    "diSymGraph_pConnecParams/41x15c/horizontal_white_bar/bar0,67x0,9°_6dps/" \
-                    "ampGang/test/oui"
+                        "diSymGraph_pConnecParams/41x15c/horizontal_white_bar/bar0,67x0,9°_6dps/" \
+                        "ampGang/test/oui"
 
     macuscript_launcher.create_non_existing_path(path_macudata_file)
     assert os.path.isdir(path_macudata_dir)
@@ -178,14 +172,15 @@ def test_path_file_to_path_dir():
     path_init_file = "../resources/config_macuscript_default.json"
     macuscript_launcher = msl.MacularScriptLauncher(path_init_file)
     path_macudata_file = "/user/jemonet/home/Documents/These/Data Macular/RefactoredMacular/" \
-                    "diSymGraph_pConnecParams/41x15c/horizontal_white_bar/bar0,67x0,9°_6dps/" \
-                    "ampGang/test/test.py"
+                         "diSymGraph_pConnecParams/41x15c/horizontal_white_bar/bar0,67x0,9°_6dps/" \
+                         "ampGang/test/test.py"
 
     path_macudata_dir = "/user/jemonet/home/Documents/These/Data Macular/RefactoredMacular/" \
-                    "diSymGraph_pConnecParams/41x15c/horizontal_white_bar/bar0,67x0,9°_6dps/" \
-                    "ampGang/test/"
+                        "diSymGraph_pConnecParams/41x15c/horizontal_white_bar/bar0,67x0,9°_6dps/" \
+                        "ampGang/test/"
 
     assert macuscript_launcher.path_file_to_path_dir(path_macudata_file) == path_macudata_dir
+
 
 def test_refresh_params_dict():
     path_init_file = "../resources/config_macuscript_default.json"
@@ -216,7 +211,6 @@ def test_load_next_sim():
 
     assert macuscript_launcher.dict_formatting_alias == dict_formatting_alias0
 
-
     assert macuscript_launcher.path_macufile == '/user/jemonet/home/Documents/These/Macular files/Session ' \
                                                 'retinocortical/RefactoredMacular/diSymGraph_pConnecParams/' \
                                                 '9x3,15°_N41x15_nue0_1,86_nui0_12,66_horizWhiteBarMotion0,67x0,9°' \
@@ -238,26 +232,23 @@ def test_make_subprocess():
 
     macuscript_launcher.load_next_sim(0)
     list_subprocess_test = ["macular-batch", "-r",
-                       "-f", '/user/jemonet/home/Documents/These/Macular files/Session '
-                             'retinocortical/RefactoredMacular/diSymGraph_pConnecParams/'
-                             '9x3,15°_N41x15_nue0_1,86_nui0_12,66_horizWhiteBarMotion0,67x0,9°'
-                             '_6dps_deltat0,0167_ampOPL0,025.json',
-                       "-o", '/user/jemonet/home/Documents/These/Data Macular/RefactoredMacular/'
-                             'diSymGraph_pConnecParams/41x15c/'
-                             'horizontal_white_bar/bar0,67x0,9°_6dps/'
-                             'ampGang/RC_RM_dSGpCP0012_ampGang1Hz_0f.csv',
-                       "-s", '/user/jemonet/home/Documents/These/stimuli/stim_'
-                             'database_newName/horizontal_motion/black_background_white_bar/'
-                             'horizontalScreen/2700x945/bar201x270/'
-                             'D187_motion30_trajX-191to2689_Y338to338_0f.mp4',
-                       "-p", "virtual_retina/relative_ampOPL=0.00083"]
+                            "-f", '/user/jemonet/home/Documents/These/Macular files/Session '
+                                  'retinocortical/RefactoredMacular/diSymGraph_pConnecParams/'
+                                  '9x3,15°_N41x15_nue0_1,86_nui0_12,66_horizWhiteBarMotion0,67x0,9°'
+                                  '_6dps_deltat0,0167_ampOPL0,025.json',
+                            "-o", '/user/jemonet/home/Documents/These/Data Macular/RefactoredMacular/'
+                                  'diSymGraph_pConnecParams/41x15c/'
+                                  'horizontal_white_bar/bar0,67x0,9°_6dps/'
+                                  'ampGang/RC_RM_dSGpCP0012_ampGang1Hz_0f.csv',
+                            "-s", '/user/jemonet/home/Documents/These/stimuli/stim_'
+                                  'database_newName/horizontal_motion/black_background_white_bar/'
+                                  'horizontalScreen/2700x945/bar201x270/'
+                                  'D187_motion30_trajX-191to2689_Y338to338_0f.mp4',
+                            "-p", "virtual_retina/relative_ampOPL=0.00083"]
 
     assert macuscript_launcher.make_subprocess() == list_subprocess_test
 
-
-#def test_run():
-    # TODO
+# def test_run():
 
 
-#def test_multiple_run():
-    # TODO
+# def test_multiple_run():
